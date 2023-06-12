28d265b815bf5127ff3e6156474b1859aeed45a5
Francisco <fra.zelada.s@gmail.com>
2023-06-12 17:18:19 -0400

    notifications management

--- src/slice/booksSlice.ts
+++ src/slice/booksSlice.ts
@@ -4,11 +4,13 @@ import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 // Define a type for the slice state
 interface BooksState {
     favBooks: BooksBase[];
+    newFavNotifications: number;
 }
 
 // Define the initial state using that type
 const initialState: BooksState = {
     favBooks: [],
+    newFavNotifications: 0,
 };
 
 export const booksSlice = createSlice({
@@ -19,9 +21,16 @@ export const booksSlice = createSlice({
         addFavBook: (state, action: PayloadAction<BooksBase>) => {
             state.favBooks = [action.payload, ...state.favBooks];
         },
+        addNewFavNotification: (state, action: PayloadAction<number>) => {
+            state.newFavNotifications += action.payload;
+        },
+        resetFavNotifications: (state, action: PayloadAction<number>) => {
+            state.newFavNotifications = action.payload;
+        },
     },
 });
 
-export const { addFavBook } = booksSlice.actions;
+export const { addFavBook, addNewFavNotification, resetFavNotifications } =
+    booksSlice.actions;
 
 export default booksSlice.reducer;
